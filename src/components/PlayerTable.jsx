cd22b9c7b1de3cba12636cb1e0ae17ea8753b493
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2025-03-09 12:08:52 +0530

    Adjust items per page in PlayerTable for mobile responsiveness

--- src/components/PlayerTable.jsx
+++ src/components/PlayerTable.jsx
@@ -26,7 +26,7 @@ const PlayerTable = ({ playerList, onAddNewPlayer, isLoading, isUser }) => {
   useEffect(() => {
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
-      setItemsPerPage(window.innerWidth <= 768 ? 5 : 14);
+      setItemsPerPage(window.innerWidth <= 768 ? 5 : 8);
     };
 
     handleResize();
