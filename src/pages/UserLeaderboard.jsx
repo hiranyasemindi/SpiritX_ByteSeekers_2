710721e2c18ce422979d7f28364589cb0e7c08c2
Tilakna Gunawardhane <140165443+tilaknagunawardhane@users.noreply.github.com>
2025-03-09 08:53:21 +0530

    Add UserLeaderboard and TeamTableUser components with tournament summary and pagination

--- src/pages/UserLeaderboard.jsx
+++ src/pages/UserLeaderboard.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
+// import TournementStats from './TournementStats'
+import TournementStats from '../components/TournementStats'
+import TeamTableUser from './TeamTableUser'
+import Tournament from '../components/TournementStats'
+
 
 export default function UserLeaderboard() {
+
+    const tournements = [
+        {
+            id: 1,
+            Name: 'Chamika Chandimal',
+            Team: 'University of the Visual & Performing Arts',
+            points: 100000,
+        },
+        {
+            id: 2,
+            Name: 'Chamika Chandimal',
+            Team: 'University of the Visual & Performing Arts',
+            points: 130,
+        },
+        {
+            id: 3,
+            Name: 'Chamika Chandimal',
+            Team: 'University of the Visual & Performing Arts',
+            points: 100,
+        },
+        {
+            id: 4,
+            Name: 'Chamika Chandimal',
+            Team: 'University of the Visual & Performing Arts',
+            points: 100,
+        },
+    ]
     return (
-        <div>Leaderboard</div>
+        <>
+            <h1 className="text-2xl font-bold mb-6">Tournement Summary</h1>
+            <TournementStats />
+            <TeamTableUser teamData={tournements} />
+        </>
     )
 }
