95eeab17720030b4f4d7c984265de267ea614ebc
Virul Nirmala Wickramasinghe <89099391+iamvirul@users.noreply.github.com>
2025-03-09 08:49:38 +0530

    Add floating chat button and integrate chatbot functionality

--- src/components/UserLayout.jsx
+++ src/components/UserLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import UserSidebar from "./UserSidebar";
 import Footer from "./Footer";
+import FloatingChatButton from "./FloatingChatbot";
 
 const Layout = ({ children }) => {
     return (
+       <>
         <div className="flex min-h-screen bg-gray-100">
             <UserSidebar />
             <div className="flex flex-col flex-1 w-full pt-10 px-10">
@@ -13,6 +15,8 @@ const Layout = ({ children }) => {
                 <Footer />
             </div>
         </div>
+        <FloatingChatButton/>
+       </>
     );
 };
 
